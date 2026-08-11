652d93a59e6db6128a874deb3984ed7019caf20c
adhamgamal5353-dot <adhamgamal5353@gmail.com>
2026-08-11 15:05:25 +0300

    Add files via upload

---
+++ lib/domain/entities/user.dart
@@ -0,0 +1,27 @@
+class User {
+  const User({
+    required this.id,
+    required this.email,
+    this.displayName,
+    this.photoUrl,
+  });
+
+  final String id;
+  final String email;
+  final String? displayName;
+  final String? photoUrl;
+
+  User copyWith({
+    String? id,
+    String? email,
+    String? displayName,
+    String? photoUrl,
+  }) {
+    return User(
+      id: id ?? this.id,
+      email: email ?? this.email,
+      displayName: displayName ?? this.displayName,
+      photoUrl: photoUrl ?? this.photoUrl,
+    );
+  }
+}
